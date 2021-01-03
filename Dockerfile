1761c4f47ee5ee88ec3a214197886a5159207f95
marwan-bel <marouan.beltaief@esprit.tn>
2021-01-03 17:55:20 +0100

    testing config

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,11 @@
+
+FROM maven:3.6.3-jdk-8-slim AS build
+WORKDIR /home/app
+COPY . /home/app
+RUN mvn -f /home/app/pom.xml clean package
+
 FROM openjdk:8-jdk-alpine
 VOLUME /tmp
-ADD target/*.jar app.jar
-ENV JAVA_OPTS="
-ENTRYPOINT [ "sh", "-c", "java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar /app.jar
+EXPOSE 8000
+COPY --from=build /home/app/target/*.jar app.jar
+ENTRYPOINT [ "sh", "-c", "java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar /app.jar" ]
---
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -0,0 +1,80 @@
+#dev-timesheet-backend-state-beltaief-marwan
+#vpc_id: vpc-786c0202
+#subnetes_id: subnet-66d68f48, subnet-ec7092a1 ,subnet-b4f6a9e8
+
+terraform {
+  backend "s3" {
+    bucket = "mybucket" # Will be overridden from build
+    key    = "path/to/my/key" # Will be overridden from build
+    region = "us-east-1"
+  }
+}
+
+resource "aws_default_vpc" "default" {
+
+}
+
+data "aws_subnet_ids" "subnets" {
+  vpc_id = aws_default_vpc.default.id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.9"
+}
+
+module "in28minutes-cluster" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "in28minutes-cluster"
+  cluster_version = "1.14"
+  subnets         = ["subnet-66d68f48", "subnet-ec7092a1"] #CHANGE
+  #subnets = data.aws_subnet_ids.subnets.ids
+  vpc_id          = aws_default_vpc.default.id
+
+  #vpc_id         = "vpc-1234556abcdef"
+
+  node_groups = [
+    {
+      instance_type = "t2.micro"
+      max_capacity  = 5
+      desired_capacity = 3
+      min_capacity  = 3
+    }
+  ]
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.in28minutes-cluster.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.in28minutes-cluster.cluster_id
+}
+
+
+# We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
+# ServiceAccount needs permissions to create deployments 
+# and services in default namespace
+resource "kubernetes_cluster_role_binding" "example" {
+  metadata {
+    name = "fabric8-rbac"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = "default"
+    namespace = "default"
+  }
+}
+
+# Needed to set the default region
+provider "aws" {
+  region  = "us-east-1"
+}
